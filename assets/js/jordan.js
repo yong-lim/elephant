d1c28e90cdbf25e4321c3d7dd2649bb7fd65652a
Yong Lim <yong.lim@gmail.com>
2024-11-26 12:14:41 +0700

    Use Google Forms to get the contact us info

--- assets/js/jordan.js
+++ assets/js/jordan.js
@@ -8,6 +8,60 @@ function formFocus() {
     .addClass('hidden');
 }
 
+function formReset() {
+  $('#alert-field').show()
+    .html("<span><p>Thank you. We'll get back to you asap.</p></span><br>");
+  document.getElementById("formID").reset();
+  setTimeout(() => {
+    $('#sendButton').show();
+    $('#alert-field').hide();
+    console.log('This alert appeared after 3 second!'); 
+  }, 2000);
+}
+
+function pleaseWaite() {
+  $('#sendButton').hide();
+  $('#alert-field').removeClass();
+  $('#alert-field').show()
+    .html("<span><p>Please wait while we're sending your message . . .</p>  <progress></progress></span>");
+}
+
+//selector from your HTML form
+function postElephant(e) {
+  //prevent the form from submiting so we can post to the google form
+  console.log("in postElephant");
+  e.preventDefault();
+  // console.log("inpostEatery");
+  pleaseWaite();
+
+  //AJAX request
+  $.ajax({
+    //The public Google Form url, but replace /view with /formResponse
+    url: 'https://docs.google.com/forms/d/e/1FAIpQLSdkaW9BK9BivxOW3VK6_RBoY6l-n08CjlW7TSWs-OwtqbJIbg/formResponse',     
+    data: $('#formID').serialize(), //Nifty jquery function that gets all the input data 
+    type: 'POST', //tells ajax to post the data to the url
+    dataType: "json", //the standard data type for most ajax requests
+    mode: 'cors',
+    statusCode: { //the status code from the POST request
+      0: function(data) { //0 is when Google gives a CORS error, don't worry it went through
+        //success
+        formReset();
+       }, 
+       200: function(data) {//200 is a success code. it went through!
+        //success
+        // $('#form-success').text('hooray! 200');
+        formReset();
+       },
+       403: function(data) {//403 is when something went wrong and the submission didn't go through
+        //error
+        $('#alert-field').show()
+          .html("<span><p><b>Oh no! something went wrong. Please let us know of your problem.</b></p></span>");
+        alert('Oh no! something went wrong. Please let us know of your problem.');
+      }
+    }  
+  });
+}
+
 function sendElephant(e) {
   e.preventDefault();
 
